9787cee56277f73a8cf45aaaf72ee1a16be66319
Lucas Bessa <34755990+lucasB97@users.noreply.github.com>
2020-07-13 18:30:46 -0300

    pagina de usuário atualizada

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@
        var senha = document.getElementById("senha");
 
        if(email.value == "lucasbessa708@gmail.com" && senha.value == "123"){
-          window.location.href = "index.html";
+          window.location.href = "pagina_usuario.html";
        }
        else{
         alert("usuário ou senha invalidos");
